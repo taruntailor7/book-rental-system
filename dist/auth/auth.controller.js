766dd129315d5fe095b54114e524ab43b6b9e8f4
Tarun Tailor <taruntailor7@gmail.com>
2024-06-20 16:33:32 +0530

    used more decorrators lie @Body @Global and etc for and working on signup logic and also made interface using dto

--- dist/auth/auth.controller.js
+++ dist/auth/auth.controller.js
@@ -8,6 +8,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AuthController = void 0;
 const common_1 = require("@nestjs/common");
@@ -16,7 +19,10 @@ let AuthController = class AuthController {
     constructor(authService) {
         this.authService = authService;
     }
-    signup() {
+    signup(dto) {
+        console.log({
+            dto,
+        });
         return this.authService.signup();
     }
     signin() {
@@ -26,8 +32,9 @@ let AuthController = class AuthController {
 exports.AuthController = AuthController;
 __decorate([
     (0, common_1.Post)('signup'),
+    __param(0, (0, common_1.Body)()),
     __metadata("design:type", Function),
-    __metadata("design:paramtypes", []),
+    __metadata("design:paramtypes", [Object]),
     __metadata("design:returntype", void 0)
 ], AuthController.prototype, "signup", null);
 __decorate([
